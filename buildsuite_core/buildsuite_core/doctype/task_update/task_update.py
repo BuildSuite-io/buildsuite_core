e042b483cff441f3bb151efe14072fb5b28a7168
Ubuntu <bdadmin@Frappe16-BuildSuiteCore-POC.uia1fnncea3enpyc5v1ik1winh.rx.internal.cloudapp.net>
2026-06-03 09:12:18 +0000

    feat:add new doctype task update

---
+++ buildsuite_core/buildsuite_core/doctype/task_update/task_update.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2026, Infraholic Innovations Pvt. Ltd and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TaskUpdate(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		blocker: DF.Check
+		blocker_detail: DF.SmallText | None
+		cumulative_progress: DF.Float
+		entry_date: DF.Date
+		narrative: DF.SmallText | None
+		skilled: DF.Int
+		task: DF.Link
+		unskilled: DF.Int
+		weather: DF.Literal["", "Clear", "Rainy", "Hot", "Cold", "Storm"]
+	# end: auto-generated types
+
+	pass
